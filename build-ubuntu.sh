568deb794d677a0dd4c7b3c456db8af4397e8022
myallod <_lek@inbox.ru>
2020-09-19 11:37:11 +0300

    Bump2

--- build-ubuntu.sh
+++ build-ubuntu.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 sudo apt update && \
-sudo apt -y install \
+sudo apt -y --force=yes install \
   autoconf automake build-essential ccache clang cmake curl frei0r-plugins-dev gawk gcc git libass-dev libva-dev libvdpau-dev libvo-amrwbenc-dev libvorbis-dev libwebp-dev libxcb1-dev libxcb-shm0-dev libxcb-xfixes0-dev libxvidcore-dev zlib1g-dev texi2html libfreetype6-dev libsdl1.2-dev libspeex-dev libssl-dev libtheora-dev libtheora-dev libvorbis-dev libopencore-amrnb-dev libopencore-amrwb-dev libtool pkg-config sendxmpp tar gettext gettext-base && \
-echo 'export PATH=/usr/lib/ccache:$PATH' >> .bashrc \
+echo 'export PATH=/usr/lib/ccache:$PATH' >> ~/.bashrc && \
 . ~/.bashrc
+
+git clone https://github.com/myallod/ffmpeg-static
+cd ffmpeg-static`
 
 
 #  libva-dev \
